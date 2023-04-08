5ee177442464132cd8e73324b1ee7426517d3222
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-04-08 21:04:15 +0200

    Example005_HelloName_project1

--- Examples/Example005_HelloName/Program.cs
+++ Examples/Example005_HelloName/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Write UserName: ");
 string username = Console.ReadLine();
 
-if (username == "Olha")
+if (username.ToLower() == "Olha") //ToLower() - переводит все символы строки в нижний регистр (это для того чтобы проверить совпадение только по буквам без учёта их регистра)
 {
     Console.WriteLine("You wery COOL, " + username + " !");
 }
