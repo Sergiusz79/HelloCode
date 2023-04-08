a0e54fd944885b17c4d943162a92cbc47b649d04
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-04-08 20:57:58 +0200

    Example005_HelloName_project

---
+++ Examples/Example005_HelloName/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write UserName: ");
+string username = Console.ReadLine();
+
+if (username == "Olha")
+{
+    Console.WriteLine("You wery COOL, " + username + " !");
+}
+else
+{
+    Console.WriteLine("Hello, " + username + " ! ");
+}
